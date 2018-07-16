214cb36b8769090a0164a0a1315ffb1353a07186
jdaeth274 <joshdaeth@yahoo.co.uk>
2018-07-16 15:20:32 +0100

    improved stream lining for brazil run

--- analysis/stream_line_brazil_run.R
+++ analysis/stream_line_brazil_run.R
@@ -28,13 +28,13 @@ brazil_fp$t_diagn_start <- 10L   # assume diagnoses starts in 1985
 
 brazil_fp$relinfectART <- 0.3
 brazil_fp$tsEpidemicStart <- 1970.5
-brazil_fp$likelihood_cd4 <- F
-brazil_fp$artinit_use <- F
+brazil_fp$likelihood_cd4 <- FALSE
+brazil_fp$artinit_use <- FALSE
 brazil_fp$time_at_which_get_cd4_counts <- 2001
 
 brazil_fp$diagn_rate <- array(0.8, c(dim(brazil_fp$cd4_mort), brazil_fp$ss$PROJ_YEARS))
-brazil_fp$likelihood_cd4 <- T
-brazil_fp$artinit_use <- F
+brazil_fp$likelihood_cd4 <- TRUE
+brazil_fp$artinit_use <- FALSE
 brazil_fp$artcd4elig_idx <- rep(1L, length(brazil_fp$artcd4elig_idx))
 brazil_fp$stages <- 4L
 
@@ -51,10 +51,10 @@ brazil$fp$art15plus_num <- art_format
 #########################################################################################################
 ## Now lets run the model with the raw diagnoses and AIDS deaths and see the output #####################
 #########################################################################################################
-brazil$fp$aidsdeaths <- T
-brazil$fp$diagnoses_uses <- T
+brazil$fp$aidsdeaths <- FALSE
+brazil$fp$diagnoses_uses <- FALSE
 brazil$fp$tARTstart <- 28L
-brazil$fp$linear_diagnosis <- TRUE
+brazil$fp$linear_diagnosis <- FALSE
 
 brazil_opt1_cd4_both <- fitmod_csavr(brazil, incid_func = "ilogistic", B0=1e3, optfit=TRUE)
 brazil_fit1_cd4_both <- fitmod_csavr(brazil, incid_func = "ilogistic", B0=1e4, B=1e3, B.re=3e3, opt_iter=1:3*5)
@@ -66,32 +66,26 @@ brazil_fit2_cd4_both <- fitmod_csavr(brazil, incid_func = "idbllogistic", B0=1e4
 brazil_opt3_cd4_both <- fitmod_csavr(brazil, eppmod="rlogistic", B0=1e4, optfit=TRUE)
 brazil_fit3_cd4_both <- fitmod_csavr(brazil, eppmod="rlogistic", B0=1e4, B=1e3, B.re=3e3, opt_iter=1:3*5)
 
-save(brazil_fit1_cd4_both,file = "C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/brazil_fit1_cd4_both")
-save(brazil_fit2_cd4_both, file = "C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/brazil_fit2_cd4_both")
-#### lets tidy the results and plot them #####
-load("C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/brazil_fit1_cd4_both",verbose = T)
-load("C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/brazil_fit2_cd4_both", verbose = T)
-
 brazil_out1_cd4_both <- tidy(brazil_fit1_cd4_both) %>% data.frame(model = "logistic", .)
 brazil_out2_cd4_both <- tidy(brazil_fit2_cd4_both) %>% data.frame(model = "double logistic", .)
 brazil_out3_cd4_both <- tidy(brazil_fit3_cd4_both) %>% data.frame(model = "rlogistic", .)
 brazil_out_cd4 <- rbind(brazil_out1_cd4_both, brazil_out2_cd4_both, brazil_out3_cd4_both)
 
 
-no_cd4_fit <- ggplot(subset(brazil_out_cd4, year %in% 1975:2017), 
+fit_output <- ggplot(subset(brazil_out_cd4, year %in% 1975:2017), 
                      aes(year, mean, ymin=lower, ymax=upper, color=model, fill=model)) +
   geom_line(size=1.05) + geom_ribbon(alpha=0.2) + 
   facet_wrap(~outcome, scales="free") +
   geom_point(aes(y=lik_data), col="darkred", size=1)
 
-save(no_cd4_fit, file = "C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/Plots/CD4_both_DIAG_DEATHS_tara_ART")
+save(fit_output, file = "C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/Plots/CD4_both_DIAG_DEATHS_tara_ART")
 save(brazil_out_cd4, file = "C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/Data/CD4_both_DIAG_DEATHS_tara_ART_DATA")
 
 ########################################################################################################
 ## NOw lets just fit it with Diagnoses #################################################################
 ########################################################################################################
-brazil$fp$aidsdeaths <- T
-brazil$fp$diagnoses_uses <- F
+brazil$fp$aidsdeaths <- TRUE
+brazil$fp$diagnoses_uses <- FALSE
 
 brazil_opt1_no_diag <- fitmod_csavr(brazil, incid_func = "ilogistic", B0=1e3, optfit=TRUE)
 brazil_fit1__no_diag <- fitmod_csavr(brazil, incid_func = "ilogistic", B0=1e4, B=1e3, B.re=3e3, opt_iter=1:3*5)
@@ -110,12 +104,8 @@ brazil_out2_no_diag <- tidy(brazil_fit2_no_diag) %>% data.frame(model = "double 
 brazil_out3_no_diag <- tidy(brazil_fit3_no_diag) %>% data.frame(model = "rlogistic", .)
 brazil_out_no_diag <- rbind(brazil_out1_no_diag, brazil_out2_no_diag, brazil_out3_no_diag)
 
-brazil_out_no_diag[is.infinite(brazil_out_no_diag$mean)] <- 0
-brazil_out_no_diag[brazil_out_no_diag$model =="logistic",]
 
-brazil_out_no_diag_no_logi <- rbind(brazil_out2_no_diag, brazil_out3_no_diag)
-
-no_cd4_fit <- ggplot(subset(brazil_out_no_diag, year %in% 1975:2017), 
+no_diag_fit <- ggplot(subset(brazil_out_no_diag, year %in% 1975:2017), 
                      aes(year, mean, ymin=lower, ymax=upper, color=model, fill=model)) +
   geom_line(size=1.05) + geom_ribbon(alpha=0.2) + 
   facet_wrap(~outcome, scales="free") +
@@ -128,8 +118,8 @@ save(brazil_out_no_diag,file = "C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/Da
 #######################################################################################################
 ## Now we will run the data with just the diagnoses ###################################################
 #######################################################################################################
-brazil$fp$aidsdeaths <- F
-brazil$fp$diagnoses_uses <- T
+brazil$fp$aidsdeaths <- FALSE
+brazil$fp$diagnoses_uses <- TRUE
 
 brazil_opt1_no_aids <- fitmod_csavr(brazil, incid_func = "ilogistic", B0=1e3, optfit=TRUE)
 brazil_fit1__no_aids <- fitmod_csavr(brazil, incid_func = "ilogistic", B0=1e4, B=1e3, B.re=3e3, opt_iter=1:3*5)
@@ -162,7 +152,7 @@ save(brazil_out_no_diag,file = "C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/Da
 ## Now lets run it with CD4 counts for diagnoses only as well #######################################################
 #####################################################################################################################
 
-brazil$fp$likelihood_cd4 <- T
+brazil$fp$likelihood_cd4 <- TRUE
 
 brazil_opt1_no_aids_cd4 <- fitmod_csavr(brazil, incid_func = "ilogistic", B0=1e3, optfit=TRUE)
 brazil_fit1__no_aids_cd4 <- fitmod_csavr(brazil, incid_func = "ilogistic", B0=1e4, B=1e3, B.re=3e3, opt_iter=1:3*5)
@@ -205,3 +195,43 @@ no_cd4_fit <- ggplot(subset(no_cd4_fit$data, year %in% 1975:2017),
   facet_wrap(~outcome, scales="free") +
   geom_point(aes(y=lik_data), col="darkred", size=1) +
   scale_y_continuous(breaks=NULL)
+
+
+##############################################################################
+## Optimizer fiddle ##########################################################
+##############################################################################
+
+brazil_opt_2_out <- attributes(brazil_opt2_cd4_both$mod)
+hiv_deaths <- colSums(brazil_opt_2_out$hivdeaths,,2)
+plot(hiv_deaths,type="l")
+points(brazil_opt2_cd4_both$dat$aids_deaths,col="red")
+
+diagnoses <- colSums(brazil_opt_2_out$diagnoses,,3)
+plot(diagnoses, type = "l", col="blue")
+indexed_diag <- c(rep(0,10),brazil_opt2_cd4_both$dat$total_cases[1],0,brazil_opt2_cd4_both$dat$total_cases[2:35])
+points(indexed_diag,col="red")
+
+
+##############################################################################
+## loading up the plots ######################################################
+##############################################################################
+
+load("C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/Plots/CD4_both_DIAG_DEATHS_tara_ART",verbose = T)
+CD4_both_diag_and_Deaths_fit <- no_cd4_fit
+CD4_both_diag_and_Deaths_fit
+
+load("C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/Plots/no_AIDSDEATHS_CD4_likel_tara_art_no_cd4_plot", verbose = T)
+cd4_diag_no_deaths_fit <- no_cd4_fit
+cd4_diag_no_deaths_fit
+
+load("C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/Plots/no_AIDSDEATHS_tara_art_no_cd4_plot", verbose = T)
+no_cd4_diag_no_deaths_fit <- no_cd4_fit
+no_cd4_diag_no_deaths_fit
+
+load("C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/Plots/no_diag_tara_art_no_cd4_plot", verbose = T)
+no_diag_DEATHS_fit <- no_cd4_fit
+no_diag_DEATHS_fit
+
+load("C:/Users/josh/Dropbox/hiv_project/EPPASM_runs/Plots/PLOT_no_cd4_tara_ART_both_DIAG_and_AIDSdeaths", verbose = T)
+no_cd4_diag_and_deaths_fit <- no_cd4_fit
+no_cd4_diag_and_deaths_fit

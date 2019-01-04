a463a77d08943f669a7203670485a58d9ebca138
Robert Ashton <r.ashton@imperial.ac.uk>
2019-01-04 16:24:00 +0000

    Add further test cases to improve code coverage

--- scripts/generate_reference_data.R
+++ scripts/generate_reference_data.R
@@ -1,5 +1,18 @@
 devtools::load_all()
-pjnz <- system.file("extdata/testpjnz", "Botswana2018.PJNZ", package="eppasm")
-bw <- prepare_spec_fit(pjnz, proj.end=2021.5)
-dir.create("tests/testthat/reference", FALSE, TRUE)
-saveRDS(bw, "tests/testthat/reference/Botswana2018.rds")
+
+test_cases = c(
+  "Botswana2017",
+  "Botswana2018",
+  "DominicanRepublic2017",
+  "Mozambique_Maputo_Cidade2018",
+  "Netherlands2017"
+)
+for (test_case in test_cases) {
+  print(test_case)
+  pjnz <- system.file("extdata/testpjnz",
+                      paste(test_case, ".PJNZ", sep = ""),
+                      package = "eppasm")
+  bw <- prepare_spec_fit(pjnz, proj.end = 2021.5)
+  dir.create("tests/testthat/reference", FALSE, TRUE)
+  saveRDS(bw, paste("tests/testthat/reference/", test_case, ".rds", sep = ""))
+}
